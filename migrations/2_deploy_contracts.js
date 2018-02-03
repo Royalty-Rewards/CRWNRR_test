c83d2a9005607e6ce079558d5ccf1699c05a0f73
T Grattan Bodkin <gratbodkin@gmail.com>
2018-02-03 01:15:40 -0800

    update dev environment
    NOTES:
    
    A lot of the files located in the src directory originated from a truffle box package.
    The truffle box utilizes reactjs, which is reflected in many of the newly committed source files.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var CRWNToken = artifacts.require("./CRWNToken.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, CRWNToken);
-  deployer.deploy(CRWNToken);
+  deployer.deploy(SimpleStorage);
 };
